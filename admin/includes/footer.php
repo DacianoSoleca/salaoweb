7dabc04fde3f3b4233e88432c6657f5986025edd
DacianoSoleca <dacianoadriano@gmail.com>
2024-07-07 22:07:32 +0100

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,5 @@
+<!--footer-->
+    <div class="footer">
+       <p>&copy; 2024 Zoya Beauty Admin Painel.</p>
+    </div>
+        <!--//footer-->
---
+++ admin/includes/sidebar.php
@@ -0,0 +1,85 @@
+  <div class=" sidebar" role="navigation">
+            <div class="navbar-collapse">
+        <nav class="cbp-spmenu cbp-spmenu-vertical cbp-spmenu-left" id="cbp-spmenu-s1">
+          <ul class="nav" id="side-menu">
+            <li>
+              <a href="dashboard.php"><i class="fa fa-home nav_icon"></i>Página Inicial</a>
+            </li>
+            <li>
+              <a href="add-services.php"><i class="fa fa-cogs nav_icon"></i>Serviços<span class="fa arrow"></span> </a>
+              <ul class="nav nav-second-level collapse">
+                <li>
+                  <a href="add-services.php">Adicionar Serviços</a>
+                </li>
+                <li>
+                  <a href="manage-services.php">Gerenciar Serviços</a>
+                </li>
+              </ul>
+              <!-- /nav-second-level -->
+            </li>
+            <li class="">
+              <a href="about-us.php"><i class="fa fa-book nav_icon"></i>Páginas <span class="fa arrow"></span></a>
+              <ul class="nav nav-second-level collapse">
+                <li>
+                  <a href="about-us.php">Sobre nós</a>
+                </li>
+                <li>
+                  <a href="contact-us.php">Encontre-nos</a>
+                </li>
+              </ul>
+              <!-- /nav-second-level -->
+            </li>
+          
+            <li>
+              <a href="all-appointment.php"><i class="fa fa-check-square-o nav_icon"></i>Marcações<span class="fa arrow"></span></a>
+              <ul class="nav nav-second-level collapse">
+                <li>
+                  <a href="all-appointment.php">Todas Marcações</a>
+                </li>
+                <li>
+                  <a href="new-appointment.php">Nova Marcação</a>
+                </li>
+                <li>
+                  <a href="accepted-appointment.php">Marcações Aceites</a>
+                </li>
+                <li>
+                  <a href="rejected-appointment.php">Marcações Rejeitadas</a>
+                </li>
+              </ul>
+              <!-- //nav-second-level -->
+            </li>
+           
+        
+            <li>
+              <a href="add-customer.php" class="chart-nav"><i class="fa fa-user nav_icon"></i>Adicionar Clientes</a>
+            </li>
+             <li>
+              <a href="customer-list.php" class="chart-nav"><i class="fa fa-users nav_icon"></i>Lista de Clientes</a>
+            </li>
+              <li>
+              <a href="#"><i class="fa fa-check-square-o nav_icon"></i>Relatórios<span class="fa arrow"></span></a>
+              <ul class="nav nav-second-level collapse">
+                 <li><a href="bwdates-reports-ds.php"> Relatórios de Serviços</a></li>
+                   
+                    <li><a href="sales-reports.php">Relatório de Vendas</a></li>
+              </ul>
+              <!-- //nav-second-level -->
+            </li>
+
+    <li>
+              <a href="invoices.php" class="chart-nav"><i class="fa fa-file-text-o nav_icon"></i>Faturas</a>
+            </li>
+            <li>
+              <a href="search-appointment.php" class="chart-nav"><i class="fa fa-search nav_icon"></i>Procurar Marcações</a>
+            </li>
+            <li>
+              <a href="search-invoices.php" class="chart-nav"><i class="fa fa-search nav_icon"></i>Procurar Faturar</a>
+            </li>
+          
+
+          </ul>
+          <div class="clearfix"> </div>
+          <!-- //sidebar-collapse -->
+        </nav>
+      </div>
+    </div>
